8673b119e07b1c70fccee1a96db7029d7bc4aeee
AmruthaMuralidhar1 <125537105+AmruthaMuralidhar1@users.noreply.github.com>
2024-04-10 19:31:48 +0530

    Create 219_Contains Duplicate.py

---
+++ 219_Contains Duplicate.py
@@ -0,0 +1,8 @@
+sliding window contains duplicate
+
+    seen = {}
+    for i, num in enumerate(nums):
+        if num in seen and i - seen[num] <= k:
+            return True
+        seen[num] = i
+    return False
